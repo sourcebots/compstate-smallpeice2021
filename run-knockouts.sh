9fe9f85c28d4fad7e2140b49d4cb8e5d0982b1a4
Peter Law <PeterJCLaw@gmail.com>
2021-08-19 22:52:24 +0100

    Add script for running the knockouts
    Not sure how useful this will be given that it's a bash script and
    I think our simulations are going to be running on Windows, but
    hopefully it's enough of a starting point for either conversion
    or at worst some copy/paste.

---
+++ run-knockouts.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SIMULATOR_ROOT="$1"
+ARCHIVES_DIR="$2"
+
+if [ -z "${SIMULATOR_ROOT}" || -z "${ARCHIVES_DIR}" ]
+then
+    echo "Usage: $0 SIMULATOR_ROOT ARCHIVES_DIR"
+    exit 1
+fi
+
+SIMULATOR_ROOT="realpath ${SIMULATOR_ROOT}"
+ARCHIVES_DIR="realpath ${ARCHIVES_DIR}"
+
+cd $(dirname $0)
+
+srcomp for-each-match . 32-35 ${SIMULATOR_ROOT}/scripts/run-comp-match ${ARCHIVES_DIR} {NUMBER} @TLAS
+
+cp ${ARCHIVES_DIR}/*.yaml league/Simulator/
+
+srcomp for-each-match . 36,37 ${SIMULATOR_ROOT}/scripts/run-comp-match ${ARCHIVES_DIR} {NUMBER} @TLAS
+
+cp ${ARCHIVES_DIR}/*.yaml league/Simulator/
+
+srcomp for-each-match . 38 ${SIMULATOR_ROOT}/scripts/run-comp-match ${ARCHIVES_DIR} {NUMBER} @TLAS
+
+cp ${ARCHIVES_DIR}/*.yaml league/Simulator/
